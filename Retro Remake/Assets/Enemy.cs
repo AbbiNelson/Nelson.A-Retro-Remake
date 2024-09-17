79875f7a11d8550830b4f4a5d6ff5aa0ea37ae73
MUKILTEO\1630107 <160107@mukilteo.wednet.edu>
2024-09-17 09:47:58 -0700

    Has enemies along with their movement scpirt

---
+++ Retro Remake/Assets/Enemy.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        movement = direction;
+        direction.Normalize();
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
